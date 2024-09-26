e8cf09e1eb5004295c6316282c40d90d20536b69
seokminlee24 <lsm67286564@gmail.com>
2024-09-26 15:55:22 +0900

    model attributes

---
+++ src/main/java/com/example/java/controller/Controller11.java
@@ -0,0 +1,116 @@
+package com.example.java.controller;
+
+import com.example.java.dto.c10.Car;
+import com.example.java.dto.c11.CarList;
+import com.example.java.dto.c11.Person;
+import com.example.java.dto.c11.Player;
+import com.example.java.dto.c11.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/main11")
+public class Controller11 {
+    @RequestMapping("sub1")
+    public String sub1(Model model) {
+        //String, 기본타입, 배열, List, map, java, javabeans
+        var data = List.of(
+                Map.of(
+                        "name", "흥민",
+                        "team", "토트넘"),
+
+                Map.of(
+                        "country", "korea",
+                        "location", "강남"),
+                Map.of(
+                        "price", 6000,
+                        "model", "truck")
+        );
+        model.addAttribute("data", data);
+        return null;
+    }
+
+    @RequestMapping("sub2")
+    public String sub2(Model model) {
+        var list1 = List.of(
+                Map.of(
+                        "team", "자이언츠",
+                        "price", 10000,
+                        "number", 4567),
+                Map.of(
+                        "location", "제주",
+                        "name", "바이트",
+                        "company", "삼성"
+                )
+        );
+        model.addAttribute("list1", list1);
+        return null;
+    }
+
+    @RequestMapping("sub3")
+    public String sub3(Model model) {
+        model.addAttribute("data", Map.of(
+                "list1", List.of("tesla", "volvo", "kia"),
+                "languages", List.of("java", "js", "c++", "python")
+        ));
+        return null;
+    }
+
+    @RequestMapping("sub4")
+    public String sub4(Model model) {
+
+        model.addAttribute("car",
+                Map.of("hyundai",
+                        List.of("소나타", "아반떼", "제네시스")));
+
+        model.addAttribute("student",
+                Map.of("son",
+                        List.of(80, 90, 99)));
+
+        return null;
+    }
+
+    @RequestMapping("sub5")
+    public String sub5(Model model) {
+        //List 내의 javaBeans
+        var students = List.of(
+                new Student(1, "son", 33, 98.1234, "gmail"),
+                new Student(5, "hong", 55, 77.33, "yahoo"),
+                new Student(7, "messi", 66, 88.44, "daum"),
+                new Student(10, "otani", 22, 66.1234, "naver")
+        );
+        model.addAttribute("students", students);
+        return null;
+    }
+
+    @RequestMapping("sub6")
+    public String sub6(Model model) {
+        model.addAttribute("carList", List.of(
+                new CarList("model x", 5000, false),
+                new CarList("model s", 4000, true),
+                new CarList("model s", 4000, true)
+        ));
+        return null;
+    }
+
+    @RequestMapping("sub7")
+    public String sub7(Model model) {
+        // JavaBeans 내의 List
+        model.addAttribute("person",
+                new Person("donald", "trump", 77, List.of("game", "soccer", "golf")));
+        return null;
+    }
+
+    @RequestMapping("sub8")
+    public String sub8(Model model) {
+        model.addAttribute("player",
+                new Player("donald", "trump", "yahoo", true,
+                        List.of("토트넘", "맨시티", "다저스"),
+                        List.of(3.14, 5.12, 6.34, 7.56)));
+        return null;
+    }
+}
