e1c5d88a0da906d913432a94654caef92ef38aca
seokminlee24 <lsm67286564@gmail.com>
2024-09-30 17:10:17 +0900

    import

---
+++ src/main/java/com/example/java/dto/c15/Product.java
@@ -0,0 +1,12 @@
+package com.example.java.dto.c15;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Product {
+    private Integer id;
+    private String name;
+    private Integer price;
+}
