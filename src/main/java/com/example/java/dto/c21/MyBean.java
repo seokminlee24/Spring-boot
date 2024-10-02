ccf160dee87511bc4d4e7cc992cd68f4624e1775
seokminlee24 <lsm67286564@gmail.com>
2024-10-02 17:40:26 +0900

    ModelAttribute

---
+++ src/main/java/com/example/java/dto/c21/MyBean.java
@@ -0,0 +1,8 @@
+package com.example.java.dto.c21;
+
+import lombok.Data;
+
+@Data
+public class MyBean {
+    private String lunch;
+}
---
+++ src/main/java/com/example/java/dto/c21/MyBean8.java
@@ -0,0 +1,29 @@
+package com.example.java.dto.c21;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class MyBean8 {
+    public MyBean8(String userName) {
+        this.userName = userName;
+    }
+
+    public MyBean8(String userName, Integer age, Double score) {
+        this.userName = userName;
+        this.age = age;
+        this.score = score;
+    }
+
+    private String userName;
+    private Integer age;
+    private List<String> foodList;
+    private LocalDate startDate;
+    private LocalDateTime endDateTime;
+    private Boolean expired;
+    private Double score;
+}
+
