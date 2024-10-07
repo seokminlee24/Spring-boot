0395f7609ad5262930db0fe8728dcffd931d8cb7
seokminlee24 <lsm67286564@gmail.com>
2024-10-07 17:02:15 +0900

    select

---
+++ sql/01select.sql
@@ -0,0 +1,36 @@
+USE w3schools; #실행 ctrl enter
+
+SELECT *
+from Customers;
+SELECT *
+from Employees;
+SELECT *
+from Products;
+SELECT *
+from Categories;
+
+# Data들은 Table에 저장됨
+# Table은 행과 열로 저장됨
+
+# 행(row) : recode
+# 열(column) : attribute. filed
+
+# SELECT : 테이블의 데이터를 조회(가져옴)
+# SELECT * from 테이블명;
+SELECT *
+from Products;
+SELECT *
+from Categories;
+SELECT *
+from Customers;
+# 나머지 6개 테이블 조회
+SELECT *
+from Employees;
+SELECT *
+from OrderDetails;
+SELECT *
+from Orders;
+SELECT *
+from Shippers;
+SELECT *
+from Suppliers;
