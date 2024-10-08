6bc839129e56aca79ad1cb47b85b6d592b21c0e6
seokminlee24 <lsm67286564@gmail.com>
2024-10-08 17:31:04 +0900

    상품 조회

---
+++ src/main/java/com/example/java/dto/c26/Product.java
@@ -0,0 +1,13 @@
+package com.example.java.dto.c26;
+
+import lombok.Data;
+
+@Data
+public class Product {
+    private String id;
+    private String name;
+    private String supplier;
+    private String category;
+    private String unit;
+    private String price;
+}
