ac314c337eaf050773bc1cbd0418f83e90cbaefc
seokminlee24 <lsm67286564@gmail.com>
2024-10-11 17:25:10 +0900

    직원 정보 수정

---
+++ src/main/java/com/example/java/dto/c28/Employee.java
@@ -0,0 +1,13 @@
+package com.example.java.dto.c28;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+    private String id;
+    private String last;
+    private String first;
+    private String birth;
+    private String photo;
+    private String notes;
+}
