528594c7825df7cd2a5773b00e5c29a7f7eef9e9
seokminlee24 <lsm67286564@gmail.com>
2024-10-14 17:18:54 +0900

    drop table

---
+++ sql/20drop.sql
@@ -0,0 +1,16 @@
+USE db1;
+
+CREATE TABLE my_table27
+(
+    col1 INT
+);
+
+# 매우주의
+DROP TABLE my_table27;
+
+# my_table28 만들고 테이블 삭제
+CREATE TABLE my_table28
+(
+    col1 INT
+);
+DROP TABLE my_table28;
