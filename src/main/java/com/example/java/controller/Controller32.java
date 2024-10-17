d7c1aceadf85cc042338aa31e15584d2bb26baa7
seokminlee24 <lsm67286564@gmail.com>
2024-10-17 17:45:20 +0900

    mybatis

---
+++ src/main/java/com/example/java/controller/Controller32.java
@@ -0,0 +1,20 @@
+package com.example.java.controller;
+
+import com.example.java.mapper.Mapper02;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("main32")
+public class Controller32 {
+    private final Mapper02 mapper02;
+
+    @GetMapping("sub1")
+    public void sub1() {
+        String name = mapper02.selectSupplierName();
+        System.out.println("name = " + name);
+    }
+}
