5d9f902a386cb20ba73968d6054f396b83eaf77d
seokminlee24 <lsm67286564@gmail.com>
2024-10-17 17:37:56 +0900

    직접 만드는 앱

---
+++ src/main/java/core/app4/App4.java
@@ -0,0 +1,46 @@
+package core.app4;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App4 {
+    public static void main(String[] args) {
+        Controller1 controller1 = new Controller1();
+        Controller2 controller2 = new Controller2();
+        Service1 service1 = new Service1();
+        controller1.setS(service1);
+        controller2.setS(service1);
+    }
+}
+
+class Controller1 {
+    Service1 s;
+
+    public void setS(Service1 s) {
+        this.s = s;
+    }
+
+    public void method1() {
+        System.out.println("요청파라미터 분석/ 가공");
+        System.out.println("CRUD");
+    }
+}
+
+class Controller2 {
+    Service1 s;
+
+    public void setS(Service1 s) {
+        this.s = s;
+    }
+
+    public void method1() {
+        System.out.println("요청 분석");
+        System.out.println("CRUD");
+    }
+}
+
+class Service1 {
+    public void crud() {
+        System.out.println("CRUD");
+    }
+}
