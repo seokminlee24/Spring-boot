e0fed08fe7825c951d9f4e97d5f2bf2d04cdc360
seokminlee24 <lsm67286564@gmail.com>
2024-10-17 17:48:19 +0900

    메소드 주입, 필드 주입

---
+++ src/main/java/core/app6/service/Service1.java
@@ -0,0 +1,12 @@
+package core.app6.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Service1 {
+
+    public void crud() {
+        System.out.println("매우 긴 crud 코드");
+    }
+
+}
