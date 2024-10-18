41798496dc21c234626e353a66c05dcf76f1b8d2
seokminlee24 <lsm67286564@gmail.com>
2024-10-18 17:26:01 +0900

    snake_case -> camelCase

---
+++ src/main/java/com/example/java/dto/c26/Supplier.java
@@ -0,0 +1,15 @@
+package com.example.java.dto.c26;
+
+import lombok.Data;
+
+@Data
+public class Supplier {
+    private String id;
+    private String name;
+    private String contactName;
+    private String address;
+    private String city;
+    private String postalCode;
+    private String country;
+    private String phone;
+}
