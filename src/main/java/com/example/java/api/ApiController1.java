5f3fba36236fe5d5151199b7db56c3eeb3235847
seokminlee24 <lsm67286564@gmail.com>
2024-11-11 16:38:36 +0900

    json 받기 react

--- src/main/java/com/example/java/api/ApiController1.java
+++ src/main/java/com/example/java/api/ApiController1.java
@@ -6,12 +6,45 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
 @RequestMapping("/api/main1")
 @Controller
 public class ApiController1 {
+    @PostMapping("sub9")
+    public void sub9(@RequestBody List<Map<String, Object>> list) {
+        // [{product: {name: computer, price: 500}, company: samsung},
+        // {product: {name: smart phone, price: 1000}, company: apple},
+        // {product: {name: graphic card, price: 2000}, company: nvidia }]
+
+        System.out.println(list);
+    }
+
+    @PostMapping("sub8")
+    public void sub8(@RequestBody List<Map<String, Object>> list) {
+        System.out.println(list);
+    }
+
+    @PostMapping("sub7")
+    public void sub7(@RequestBody List<Object> list) {
+        System.out.println(list);
+    }
+
+    @PostMapping("sub6")
+    public void sub6(@RequestBody Map<String, Object> map) {
+        System.out.println(map);
+    }
+
+    @PostMapping("sub4")
+    public void sub4(@RequestBody Map<String, Object> map) {
+
+        // {title: 책제목, content: 책내용, price: 5000}
+        System.out.println(map);
+
+    }
+
     @PostMapping("sub3")
     public void sub3(@RequestBody Map<String, String> map) {
         System.out.println(map);
