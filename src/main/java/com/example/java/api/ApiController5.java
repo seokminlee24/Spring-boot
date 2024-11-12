2c0d8c9813ab9fd8620bea329eb0c5e682d490a2
seokminlee24 <lsm67286564@gmail.com>
2024-11-12 12:21:13 +0900

    response body json 받기 react

--- src/main/java/com/example/java/api/ApiController5.java
+++ src/main/java/com/example/java/api/ApiController5.java
@@ -5,9 +5,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Map;
+
 @Controller
 @RequestMapping("/api/main5")
 public class ApiController5 {
+
+    @GetMapping("sub5")
+    @ResponseBody
+    public Map<String, Object> sub5() {
+        return Map.of("title", "책제목", "content", "책 내용",
+                "price", 15000, "available", true);
+    }
+
+    @GetMapping("sub4")
+    @ResponseBody
+    public Map<String, Object> sub4() {
+        return Map.of("name", "kim", "age", 44);
+    }
+
     @GetMapping("sub3")
     @ResponseBody
     public String method3() {
